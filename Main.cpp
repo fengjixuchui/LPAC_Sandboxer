c67e6ff6428b03113619d51f953de663d3e7a297
N.Zerof <xtahi0nix@gmail.com>
2019-01-28 02:05:43 +0300

    fix

--- Main.cpp
+++ Main.cpp
@@ -8,9 +8,9 @@ int main(int argc, char *argv[])
 	printf("Usage: lpac.exe <path to your file>\n");
 
 	if(argc == 2)
-    Sandboxed(argv[1]);
+    		Sandboxed(argv[1]);
 
 	system("pause");
 
-    return 0;
+  return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LPAC_Sandboxer
+
+Sandboxer with Windows LPAC
+Usage: lpac.exe <path to your file>
